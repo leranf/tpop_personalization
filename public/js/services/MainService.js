98b16154de2a1c33b126bf28b6caa14668130af9
Leran Firer <leranf@gmail.com>
2016-07-15 13:30:14 -0700

    adds functions for API requests to get draft wraps and create personalized wraps

--- public/js/services/MainService.js
+++ public/js/services/MainService.js
@@ -12,6 +12,22 @@ angular.module('MainService', []).factory('Main', ['$http', function($http) {
       return $http.get('/api/cards/collections/' + id).then(function(cards) {
         return cards.data;
       });
+    },
+
+    getDraftWraps: function() {
+      return $http.get('/api/wraps/drafts/').then(function(wraps) {
+        return wraps.data;
+      });
+    },
+
+    createWrap: function(personalizationInfo) {
+      return $http({
+        method: 'POST',
+        url: '/api/wraps/personalize',
+        data: personalizationInfo
+      }).then(function(wrap) {
+        return wrap.data[0];
+      });
     }
 
   }       
