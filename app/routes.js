eadfa4de0ff4bfe40f0515bc1d1feadf4e104e2d
Leran Firer <leranf@gmail.com>
2016-07-15 13:26:59 -0700

    adds routes for getting wrap drafts and creating personalized wraps

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,15 @@
 var CollectionController = require('./controllers/collectionController');
 var CardController = require('./controllers/cardController');
+var WrapController = require('./controllers/wrapController');
 
 module.exports = function(app) {
 
-  // server routes
+  // get routes
   app.get('/api/collections', CollectionController.getCollections);
   app.get('/api/cards/collections/:id', CardController.getCardsInCollection);
+  app.get('/api/wraps/drafts', WrapController.getDraftWraps);
 
+  // post routes
+  app.post('/api/wraps/personalize', WrapController.createWrap);
+  
 };
