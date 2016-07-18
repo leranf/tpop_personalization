180912c6225716d5d1383631f6f9f1ddca504e5a
Leran Firer <leranf@gmail.com>
2016-07-18 12:57:08 -0700

    passes all inputs to createWrap function

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,8 +3,7 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, Mai
   var draftWrapForPersonalization;
   Main.getDraftWraps().then(function(drafWraps) {
     draftWrapForPersonalization = drafWraps.filter(function(wrap) {
-      // return wrap.name === 'tpop';
-      return wrap.id === '01c87a37-6bec-46dd-b56c-c26525156e84';
+      return wrap.id === 'e117e2f3-babe-48da-a071-c63cc6e80618';
     })[0];
 
     Main.getCollections().then(function(collections){
@@ -23,39 +22,21 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, Mai
     });
   });
 
-
-  // $scope.plans = ['PREPAID', 'POST PAID'];
-  // $scope.details = ['2GB', '6GB', '10GB', 'UNLIMITED']
-  // $scope.phones = [
-  //   'LG G5',
-  //   'iPhone 5s',
-  //   'LG G Stylo',
-  //   'Samsung GALAXY Grand PRIME',
-  //   'Samsung GALAXY Core PRIME',
-  //   'Coolpad Rogue',
-  //   'iPhone 6',
-  //   'iPhone 6s',
-  // ];
-  // $scope.accessories = [
-  //   'OtterBox Commuter Series',
-  //   'Beats Solo 2 Headphones',
-  //   'OtterBox Defender Series'
-  // ];
-
- 
-
   $scope.createWrap = function() {
+    $scope.wrapCreated = false;
     var personalizationInfo = {
       draftWrap: draftWrapForPersonalization,
-      selectedPlan: JSON.parse($scope.selectedPlan),
-      selectedPlanDetails: JSON.parse($scope.selectedPlanDetails),
+      selectedHomePage: JSON.parse($scope.selectedHomePage),
+      selectedCoversationPage: JSON.parse($scope.selectedCoversationPage),
       selectedPhone: JSON.parse($scope.selectedPhone),
-      firstSelectedAccessory: JSON.parse($scope.firstSelectedAccessory),
-      secondSelectedAccessory: JSON.parse($scope.secondSelectedAccessory),
-      thirdSelectedAccessory: JSON.parse($scope.thirdSelectedAccessory)
+      selectedPlan: JSON.parse($scope.selectedPlan),
+      selectedAccessory: JSON.parse($scope.selectedAccessory),
+      selectedContactPage: JSON.parse($scope.selectedContactPage)
     };
+
     Main.createWrap(personalizationInfo).then(function(wrap) {
-      console.log(wrap.canonicalUrl);
+      $scope.wrapCreated = true;
+      $scope.newPersonalizedWrap = wrap.canonicalUrl;
     });
   };
 
