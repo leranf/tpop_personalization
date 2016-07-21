7757144515217302fdc7cc0b074305128a8afe96
Leran Firer <leranf@gmail.com>
2016-07-21 15:12:22 -0700

    move api key to config file

--- app/controllers/wrapController.js
+++ app/controllers/wrapController.js
@@ -1,5 +1,5 @@
 var rp = require('request-promise');
-var API_KEY = 'df57e9a5e9388115806ba189c50b6eab5eb984a1f6176254fe2ae73588ab965a';
+var API_KEY = require('../../config/config').API_KEY;
 
 module.exports = {
 
--- public/js/services/MainService.js
+++ public/js/services/MainService.js
@@ -1,5 +1,4 @@
 angular.module('MainService', []).factory('Main', ['$http', function($http) {
-  var API_KEY = 'df57e9a5e9388115806ba189c50b6eab5eb984a1f6176254fe2ae73588ab965a';
   
   return {
     
