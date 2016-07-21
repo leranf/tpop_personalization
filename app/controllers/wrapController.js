3542e2eb3861bc387feadfbd33022797d7989c0b
Leran Firer <leranf@gmail.com>
2016-07-21 15:02:11 -0700

    moves API calls to server side for creating and sharing wraps

--- app/controllers/wrapController.js
+++ app/controllers/wrapController.js
@@ -1,11 +1,11 @@
 var rp = require('request-promise');
-var API_KEY = '4b92e586d087ef6e23dac3d59c456f4c8b471df4fd34d37f2b5c57bc072b6c75';
+var API_KEY = 'df57e9a5e9388115806ba189c50b6eab5eb984a1f6176254fe2ae73588ab965a';
 
 module.exports = {
 
   getDraftWraps: function(req, res) {
     var options = {
-      uri: 'https://wrapi.qa.wrapdev.net/api/wraps',
+      uri: 'https://api.wrap.co/api/wraps',
       headers: {
         'Authorization': 'Bearer ' + API_KEY,
         'Content-Type': 'application/json'
@@ -13,9 +13,13 @@ module.exports = {
       json: true
     };
 
-    rp(options).then(function(wraps) {
-      res.json(wraps);
-    });
+    rp(options)
+      .then(function(wraps) {
+        res.json(wraps);
+      })
+      .catch(function(err) {
+        console.log(err.message);
+      });
   },
 
   createWrap: function(req, res) {
@@ -33,38 +37,39 @@ module.exports = {
       }
     }
 
-    var createWrapOptions = {
+    var createOptions = {
       method: 'POST',
-      uri: 'https://wrapi.qa.wrapdev.net/api/wraps/' + draftWrapId + '/personalize/v2',
+      uri: 'https://api.wrap.co/api/wraps/' + draftWrapId + '/personalize/v2',
       headers: {
         'Authorization': 'Bearer ' + API_KEY,
         'Content-Type': 'application/json'
       },
-      form: {
-        'personalized_json': personalized_json
-      }
-      // json: true
+      body: {
+        personalized_json: personalized_json
+      },
+      json: true
     };
 
-    rp(createWrapOptions).then(function(wrap) {
-      // var shareWrapOptions = {
-      //   uri: 'https://api.wrap.co/api/wraps/' + wrap[0].id + '/share',
-      //   headers: {
-      //     'Authorization': 'Bearer ' + API_KEY,
-      //     'Content-Type': 'application/json'
-      //   },
-      //   qs: {
-      //     type: 'sms',
-      //     phone_number: '4155890053'
-      //   }
-      // };
+    rp(createOptions)
+      .then(function(wrap) {
+        res.json(wrap);
+      });  
+  },
 
-      // rp(shareWrapOptions).then(function() {
-      //   res.json(wrap);
-      // });
-      console.log(wrap[0]);
-      res.json(wrap[0]);
-    });
+  shareWrap: function(req, res) {
+    var shareOptions = {
+      method: 'POST',
+      url: 'https://api.wrap.co/api/wraps/' + req.body.wrap.id + '/share?type=sms&phone_number=' + req.body.phoneNumber,
+      headers: {
+        'Authorization': 'Bearer ' + API_KEY,
+        'Content-Type': 'application/json'
+      }
+    }
+
+    rp(shareOptions)
+      .then(function() {
+        res.send('shared'); 
+      });
   }
 
 }
--- public/js/services/MainService.js
+++ public/js/services/MainService.js
@@ -1,5 +1,5 @@
 angular.module('MainService', []).factory('Main', ['$http', function($http) {
-  var API_KEY = '4b92e586d087ef6e23dac3d59c456f4c8b471df4fd34d37f2b5c57bc072b6c75';
+  var API_KEY = 'df57e9a5e9388115806ba189c50b6eab5eb984a1f6176254fe2ae73588ab965a';
   
   return {
     
@@ -16,50 +16,23 @@ angular.module('MainService', []).factory('Main', ['$http', function($http) {
     },
 
     getDraftWraps: function() {
-      return $http.get('/api/wraps/drafts/').then(function(wraps) {
+      return $http.get('/api/wraps/drafts').then(function(wraps) {
         return wraps.data;
       });
     },
 
     createWrap: function(personalizationInfo) {
-      var draftWrapId;
-      var personalized_json = [];
-      for (var key in personalizationInfo) {
-        if (key === 'draftWrap') {
-          draftWrapId = personalizationInfo[key].id;
-        } else {
-          var cardId = personalizationInfo[key].id;
-          personalized_json.push({
-            id: cardId,
-            data: { d: 'd' }
-          });
-        }
-      }
-
-      return $http({
-        method: 'POST',
-        url: 'https://wrapi.qa.wrapdev.net/api/wraps/' + draftWrapId + '/personalize/v2',
-        headers: {
-          'Authorization': 'Bearer ' + API_KEY,
-          'Content-Type': 'application/json'
-        },
-        data: {
-          'personalized_json': personalized_json
-        }
-      }).then(function(wrap) {
+      return $http.post('/api/wraps/personalize', personalizationInfo).then(function(wrap) {
         return wrap.data;
-      });  
+      });
     },
 
     shareWrap: function(wrap, phoneNumber) {
-      return $http({
-        method: 'POST',
-        url: 'https://wrapi.qa.wrapdev.net/api/wraps/' + wrap.id + '/share?type=sms&phone_number=' + phoneNumber,
-        headers: {
-          'Authorization': 'Bearer ' + API_KEY,
-          'Content-Type': 'application/json'
-        },
-      }).then(function() {
+      var data = {
+        wrap: wrap,
+        phoneNumber: phoneNumber
+      };
+      return $http.post('/api/wraps/share', data).then(function() {
         var numbers = phoneNumber.split('');
         var areaCode = numbers.slice(0,3).join('');
         var prefix = numbers.slice(3,6).join('');
