9397797f2b4b00313878bedc37b19ad3fbb51b0c
Leran Firer <leranf@gmail.com>
2016-07-21 15:01:15 -0700

    adds server routes for creating and sharing wraps

--- app/routes.js
+++ app/routes.js
@@ -8,8 +8,8 @@ module.exports = function(app) {
   app.get('/api/collections', CollectionController.getCollections);
   app.get('/api/cards/collections/:id', CardController.getCardsInCollection);
   app.get('/api/wraps/drafts', WrapController.getDraftWraps);
-
+  
   // post routes
   app.post('/api/wraps/personalize', WrapController.createWrap);
-  
+  app.post('/api/wraps/share', WrapController.shareWrap);
 };
