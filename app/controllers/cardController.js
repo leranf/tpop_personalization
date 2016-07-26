6e6db221199824e4c349fd7eb1722f7125d82d52
Leran Firer <leranf@gmail.com>
2016-07-26 15:55:33 -0700

    adds per page query param

--- app/controllers/cardController.js
+++ app/controllers/cardController.js
@@ -7,10 +7,11 @@ module.exports = {
     var options = {
       uri: 'https://api.wrap.co/api/cards/collections/search',
       qs: {
-        'card_collection_ids': req.params.id
+        card_collection_ids: req.params.id,
+        per_page: 100
       },
       headers: {
-        'Authorization': 'Bearer ' + API_KEY,
+        Authorization: 'Bearer ' + API_KEY,
         'Content-Type': 'application/json'
       },
       json: true
