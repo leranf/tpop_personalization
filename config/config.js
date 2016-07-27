4b813e2d4cea40d192891c137dd8887efddb1690
Leran Firer <leranf@gmail.com>
2016-07-27 14:52:52 -0700

    ignores config.js

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // TMO
-  API_KEY : 'bd2fbed80fc3a7e2e2661691b5753b6bf34017656b0c5b1cb20c112c8ddda397'
-  // my key
-  // API_KEY : 'df57e9a5e9388115806ba189c50b6eab5eb984a1f6176254fe2ae73588ab965a'
-
-}
